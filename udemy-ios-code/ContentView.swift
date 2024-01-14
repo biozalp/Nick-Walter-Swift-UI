1ae06b6021190f64b93e14c3ea8b2e0be0599dbf
biozalp <biozalp@biozalp.com>
2024-01-14 06:08:59 +0300

    Second Commit

--- udemy-ios-code/ContentView.swift
+++ udemy-ios-code/ContentView.swift
@@ -3,7 +3,7 @@
 //  udemy-ios-code
 //
 //  Created by Berk Ilgar Özalp on 14.01.2024.
-//  This project is a follow along code of mine learning SwiftUI via the Udemy lecture of Nick Walter. 
+//  This project is a follow along code of mine learning SwiftUI via the Udemy lecture of Nick Walter.
 //
 
 import SwiftUI
@@ -11,12 +11,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Merhaba cnm")
         }
-        .padding()
     }
 }
 
