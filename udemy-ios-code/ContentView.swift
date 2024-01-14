54ffbc02fae22440058504b443e7a84628e37627
biozalp <biozalp@gmail.com>
2024-01-14 05:50:34 +0300

    Initial Repo

--- udemy-ios-code/ContentView.swift
+++ udemy-ios-code/ContentView.swift
@@ -3,6 +3,7 @@
 //  udemy-ios-code
 //
 //  Created by Berk Ilgar Özalp on 14.01.2024.
+//  This project is a follow along code of mine learning SwiftUI via the Udemy lecture of Nick Walter. 
 //
 
 import SwiftUI
