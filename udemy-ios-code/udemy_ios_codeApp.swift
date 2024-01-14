998d0d8f07c7fb264cebdda107c21689851ba1e8
biozalp <biozalp@gmail.com>
2024-01-14 05:48:35 +0300

    Initial Commit

---
+++ udemy-ios-code/udemy_ios_codeApp.swift
@@ -0,0 +1,17 @@
+//
+//  udemy_ios_codeApp.swift
+//  udemy-ios-code
+//
+//  Created by Berk Ilgar Özalp on 14.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct udemy_ios_codeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
